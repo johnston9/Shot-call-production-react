c6849ceeee62ef6f8d955cac181f64241ee6ebf4
Argha Chandra Das <arghad@capitalnumbers.com>
2023-07-26 19:12:48 +0530

    csrf token added

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 /* File to contain the Axios base and create the
    axiosReq and axiosRes */
 import axios from "axios"
+import { getCookie } from "../utils/utils"
 
 axios.defaults.baseURL = "http://dev81.developer24x7.com:9100/"
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data"
@@ -9,12 +10,21 @@ axios.defaults.withCredentials = true
 export const axiosReq = axios.create()
 export const axiosRes = axios.create()
 
+const csrfToken = getCookie("csrftoken")
+
+// Create headers object with or without the Authorization header
+const csrfHeader = {}
+if (csrfToken) {
+  csrfHeader["X-CSRFToken"] = csrfToken
+}
+
 // For Protect API route use this api instance of axios
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    ...csrfHeader,
   },
   withCredentials: true,
 })
@@ -23,6 +33,7 @@ export const axiosInstanceFormData = axios.create({
   headers: {
     "Content-Type": "multipart/form-data",
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    ...csrfHeader,
   },
   withCredentials: true,
 })
@@ -32,6 +43,7 @@ export const axiosInstanceNoAuth = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    ...csrfHeader,
   },
   withCredentials: true,
 })
@@ -39,6 +51,7 @@ export const axiosInstanceNoAuthFormData = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "multipart/form-data",
+    ...csrfHeader,
   },
   withCredentials: true,
 })
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -66,3 +66,9 @@ export const unfollowHelper = (profile, clickedProfile) => {
     : // return unchanged
       profile
 }
+
+export const getCookie = (name) => {
+  const value = `; ${document.cookie}`
+  const parts = value.split(`; ${name}=`)
+  if (parts.length === 2) return parts.pop().split(";").shift()
+}
