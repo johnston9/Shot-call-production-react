ddb786dd1ee3d316d6eff3a5ac4962b754eedf54
Argha Chandra Das <arghad@capitalnumbers.com>
2024-10-22 14:18:05 +0530

    check

--- src/pages/accounts/Projects.js
+++ src/pages/accounts/Projects.js
@@ -69,7 +69,7 @@ const Projects = ({
 
         console.log(data);
 
-        setProjects(data.results);
+        setProjects(data?.data?.results);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
